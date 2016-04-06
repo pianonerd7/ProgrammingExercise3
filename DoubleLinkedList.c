841f7964b6b3c246299e077cb3470627214b2cb3
pianonerd7 <pianonerd7@gmail.com>
2016-04-06 13:03:23 -0400

    removed public

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -5,45 +5,83 @@ struct List;
 struct Node;
 
 typedef struct Node {
-	int element;
-	struct Node *next;
-	struct Node *previous;
+  int element;
+  struct Node *next;
+  struct Node *previous;
 } Node;
 
 typedef struct List {
-	struct Node *firstNode;
-	struct Node *lastNode;
+  struct Node *firstNode;
+  struct Node *lastNode;
 } List;
 
-add_to_front(int element, List *list) {
-	Node *node = malloc(sizeof(Node));
-	node->element = element;
-	if (list->firstNode == null) {
-		list->firstNode = node;
-		list->lastNode = node;
-	}
-	else {
-		node->next = list->firstNode;
-		list->firstNode = node;
-	}
-	list -> firstNode;
+void add_to_front(int element, List *list);
+void add_to_back(int element, List *list);
+int remove_from_front(List *list);
+int remove_from_back(List *list);
+void transfer();
+
+void add_to_front(int element, List *list) {
+  Node *node = malloc(sizeof(Node));
+  node->element = element;
+  if (list->firstNode == null) {
+    list->firstNode = node;
+    list->lastNode = node;
+  }
+  else {
+    node->next = list->firstNode;
+    list->firstNode = node;
+  }
+  list -> firstNode;
 }
 
-add_to_back(int element, List *list) {
-	Node *node = malloc(sizeof(Node));
-	node->element = element;
+void add_to_back(int element, List *list) {
+  Node *node = malloc(sizeof(Node));
+  node->element = element;
 
-	if (list->firstNode == null) {
-		list->firstNode = node;
-		list->lastNode = node;
-	}
-	else {
-		list->lastNode->next = node;
-		node.previous = list->lastNode;
-		list->lastNode = node;
-	}
+  if (list->firstNode == null) {
+    list->firstNode = node;
+    list->lastNode = node;
+  }
+  else {
+    list->lastNode->next = node;
+    node.previous = list->lastNode;
+    list->lastNode = node;
+  }
 }
 
-remove_from_front
-remove_from_back
-transfer
+int remove_from_front(List *list) {
+
+  if (list->firstNode == null) {
+    return null;
+  }
+
+  Node oldFirst = list->firstNode;
+  int value = oldFirst->value;
+  list->firstNode = list->firstNode->next;
+  free(oldFirst);
+  return value;
+}
+
+int remove_from_back(List *list) {
+
+  if (list->firstNode == null) {
+    return null;
+  }
+
+  return 1;
+}
+
+void transfer() {
+
+}
+
+void main() {
+  /*
+  List list = {null, null};
+  add_to_front(list, 1);
+  add_to_back(list, 2);
+
+  printf("first: " + list->element);
+  printf("second: " + list->next->element);*/
+}
