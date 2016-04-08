01fddfdf7d598931ac8ee444c4de39acb546852c
pianonerd7 <pianonerd7@gmail.com>
2016-04-07 22:30:34 -0400

    flatten is probably wrong but im saving bc my mac is giving me problems

--- HaskellTypes.hs
+++ HaskellTypes.hs
@@ -1,5 +1,16 @@
+data Nestedlist a = Element a | SubList [Nestedlist a] deriving (Show, Eq)
 
-data Nestedlist a = Element a | List [Nestedlist a] deriving (Eq, Show)
+flatten [] = []
+flatten l = 
+	if (head (head l)) == null
+		(head l) : flatten (tail l)
+	else
+		flatten (head l) : flatten (tail l)
 
+
+
+
+flatten [Element 1,Element 3,SubList [Element 4,SubList [SubList [Element 5],SubList []]],Element 6]
+[Element 1,Element 3,Element 4,Element 5,Element 6]
 
 
