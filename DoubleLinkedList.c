6cb996346cac75fb729ac830c495d714db54390b
Anna He <pianonerd7@gmail.com>
2016-04-09 16:49:14 -0400

    double free or corruption

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -68,76 +68,68 @@ int remove_from_back(List *list) {
   if (list->firstNode == NULL) {
     return -1;
   }
-  
-  int oneElement = 0;
-  if (list->lastNode->previous == NULL) {
-  	oneElement = 1;
-  }
 
   Node *oldLast = list->lastNode;
   int value = oldLast->element;
   list->lastNode = list->lastNode->previous;
   
   free(oldLast);
-  
-  if (oneElement) {
-  	free(list);
-  	printf("SET TO NULL\n");
-  }
-  
   return value;
 }
 
 void transfer(int *arr1, int *arr2, int arrLength, void (*insertFunction)(int, List *), int (*removeFunction)(List *)) {
-	
+	printf("transfer enter...");
 	List *newList = malloc(sizeof(List));
+	printf("made new list...");
 	
 	int i;
 	for (i = 0; i < arrLength; i++) {
 		insertFunction(arr1[i], newList);
 	}
-	
-	
-	int index = 0;
-	while (newList != NULL) {
-	printf("safe1\n");
-		//arr2[index] = removeFunction(newList);
-		//printf("%d \n", arr2[index]);
-		printf("%d \n", removeFunction(newList));
-		index++;
-		printf("safe2\n");
-	}
-	
-	/*
+
 	for (i = 0; i < arrLength; i++) {
 		arr2[i] = removeFunction(newList);
-	} */
+		printf("%d \n", arr2[i]);
+	} 
 	
-	//free(newList);
+	free(newList);
 }
 
 int main() {
 	int i;
 	int arrLength = 5;
 	
-	//int A3[] = {3, 4, 5, 6, 7};
-	//int A4[] = {4, 5, 6, 7, 8};
-	//int B3[5];
-	//int B4[5];
-	
-	/*
 	int A1[] = {1, 2, 3, 4, 5};
 	int B1[5];
 	transfer(A1, B1, arrLength, add_to_front, remove_from_front);
 	for (i = 0; i < arrLength; i++) {
 		printf("%d \n", B1[i]);
-	}*/
+	}
 
-	
+	printf("safe \n\n");
+
 	int A2[] = {2, 3, 4, 5, 6};
 	int B2[5];
 	transfer(A2, B2, arrLength, add_to_back, remove_from_back);
 	for (i = 0; i < arrLength; i++) {
 		printf("%d \n", B2[i]);
+	}
+	/*
+	printf("safe \n\n");
+
+	int A3[] = {3, 4, 5, 6, 7};
+	int B3[5];
+	transfer(A3, B3, arrLength, add_to_front, remove_from_back);
+	for (i = 0; i < arrLength; i++) {
+		printf("%d \n", B3[i]);
+	}*/
+	
+	printf("safe \n\n");
+	
+	int A4[] = {4, 5, 6, 7, 8};
+	int B4[5];
+	transfer(A4, B4, arrLength, add_to_back, remove_from_front);
+	for (i = 0; i < arrLength; i++) {
+		printf("%d \n", B4[i]);
 	}
 }
