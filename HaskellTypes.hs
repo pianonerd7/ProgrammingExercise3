234f1609b0074da799ec05f93003272859720655
Anna He <pianonerd7@gmail.com>
2016-04-10 01:36:02 -0400

    progress for today

--- HaskellTypes.hs
+++ HaskellTypes.hs
@@ -1,16 +1,23 @@
-data Nestedlist a = Element a | SubList [Nestedlist a] deriving (Show, Eq)
+data Nestedlist a = Element a | 
+										SubList [Nestedlist a] deriving (Show, Eq)
 
-flatten [] = []
-flatten l = 
-	if (head (head l)) == null
-		(head l) : flatten (tail l)
-	else
-		flatten (head l) : flatten (tail l)
+--flatten (SubList []) = []
+--flatten (SubList (x:xs)) = flatten x ++ flatten(SubList xs)
+--flatten l = (head l) : flatten (SubList (tail l))
+--flatten (SubList l) = flatten (head l) ++ flatten(tail l)
 
+flatten (SubList []) = []
+flatten (Element e) = [Element e]
+flatten (SubList (x:xs)) = flatten x ++ flatten(SubList xs)
+flatten (x:xs) = flatten x ++ flatten (SubList xs)
+	
 
+myreverse (SubList []) = []
+myreverse (Element e) = [Element e]
+--myreverse (SubList (x:xs)) = myreverse (SubList xs) : myreverse x :[]
 
+--random = 
 
-flatten [Element 1,Element 3,SubList [Element 4,SubList [SubList [Element 5],SubList []]],Element 6]
-[Element 1,Element 3,Element 4,Element 5,Element 6]
-
+--flatten (SubList[Element 1, Element 3, SubList[Element 4, SubList [SubList [Element 5], SubList[]]], Element 6])
+--[Element 1,Element 3,Element 4,Element 5,Element 6]
 
