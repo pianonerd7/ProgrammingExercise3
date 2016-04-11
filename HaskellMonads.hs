c557ac426d8a2dda9dc568c24f7a0f9e3363ad20
pianonerd7 <jxh604@case.edu>
2016-04-11 13:26:09 -0400

    this is probably wrong too

--- HaskellMonads.hs
+++ HaskellMonads.hs
@@ -1,26 +1,31 @@
 extract :: Maybe [a]->[a]
 extract (Just list) = list
-extract (Nothing) = []
+--extract (Nothing) = []
 
 myfunction :: a->Maybe [a]->(a->Bool)->Maybe [a]
 myfunction value list function = do
-	if (function value)
-		then 
-			Just ((extract list) ++ [value])
-		else 
-			Nothing
+  if (function value)
+    then
+      Just ((extract list) ++ [value])
+    else
+      Nothing
 
 checklist :: Eq a=>[a]->(a->Bool)->Maybe [a]
-checklist [] function return = Just []
---checklist list function = (myfunction (head list) (Just []) function) ++ (checklist (tail list) function)
+checklist [] function = Just []
 --checklist list function = Just (extract (myfunction (head list) (Just []) function) ++ extract (checklist (tail list) function))
+--checklist list function = Just (extract (myfunction (head list) (Just []) function) ++ extract (myfunction (head list) (Just[]) function))
 
-checklist list function return = 
-	if (myfunction (head list) (Just []) function) == Nothing
-		then
-			Nothing
-		else
-			checklist (tail list) function (\v->return (Just (extract (myfunction (head list) (Just []) function) ++ extract (v))))
-			
-			
+checklist list function = 
+  if (myfunction (head list) (Just []) function) == Nothing
+    then
+      Nothing
+    else
+      Just (extract (myfunction (head list) (Just []) function) ++ extract (checklist (tail list) function))
 
+mychecklist :: Eq a=>[a]->(a->Bool)->(v->v)->Maybe [a]
+mychecklist list function return =
+  if (myfunction (head list) (Just []) function) == Nothing
+    then 
+      return Nothing
+    else
+      checklist (tail list) function (\v-> return (Just (extract (myfunction (head list) (Just []) function)) ++ (extract v)))
