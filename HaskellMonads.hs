ff68f658e6b458630614244e57740fb735a8b545
Anna He <pianonerd7@gmail.com>
2016-04-11 02:07:24 -0400

    i cant anymore

--- HaskellMonads.hs
+++ HaskellMonads.hs
@@ -1,6 +1,6 @@
 extract :: Maybe [a]->[a]
 extract (Just list) = list
---extract (Nothing) = Nothing
+extract (Nothing) = []
 
 myfunction :: a->Maybe [a]->(a->Bool)->Maybe [a]
 myfunction value list function = do
@@ -10,9 +10,17 @@ myfunction value list function = do
 		else 
 			Nothing
 
-checklist :: [a]->(a->Bool)->Maybe [a]
-checklist [] function = Just []
-checklist list function = Just (extract (myfunction (head list) (Just []) function) ++ extract (checklist (tail list) function))
---checklist list function = Just (extract (myfunction (head list) (Just []) function) ++ extract (myfunction (head list) (Just[]) function))
+checklist :: Eq a=>[a]->(a->Bool)->Maybe [a]
+checklist [] function return = Just []
+--checklist list function = (myfunction (head list) (Just []) function) ++ (checklist (tail list) function)
+--checklist list function = Just (extract (myfunction (head list) (Just []) function) ++ extract (checklist (tail list) function))
 
+checklist list function return = 
+	if (myfunction (head list) (Just []) function) == Nothing
+		then
+			Nothing
+		else
+			checklist (tail list) function (\v->return (Just (extract (myfunction (head list) (Just []) function) ++ extract (v))))
+			
+			
 
